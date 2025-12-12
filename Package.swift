f738540957eb7d23198e5be5560c8c9a454ccfb7
Tim <0xtimc@gmail.com>
2025-12-12 22:09:51 +0000

    Bump the versions for now

--- Package.swift
+++ Package.swift
@@ -3,6 +3,14 @@ import PackageDescription
 
 let package = Package(
     name: "auth",
+    platforms: [
+        .macOS(.v26),
+        .macCatalyst(.v26),
+        .iOS(.v26),
+        .tvOS(.v26),
+        .watchOS(.v26),
+        .visionOS(.v26),
+    ],
     products: [
         .library(name: "Authentication", targets: ["Authentication"]),
     ],
