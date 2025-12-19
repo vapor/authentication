e99c250eb96b4f7b0774ce08fd947fdd79770392
Tim <0xtimc@gmail.com>
2025-12-19 18:48:22 +0000

    Sort out public enum

--- Sources/Authentication/Passwords/Bcrypt/VaporBcrypt.swift
+++ Sources/Authentication/Passwords/Bcrypt/VaporBcrypt.swift
@@ -215,37 +215,6 @@ public enum VaporBcrypt: Sendable {
     }
 }
 
-public enum BcryptError: Swift.Error, CustomStringConvertible, LocalizedError, Sendable {
-    case invalidCost
-    case invalidSalt
-    case hashFailure
-    case invalidHash
-    case internalError
-
-    public var errorDescription: String? {
-        return self.description
-    }
-
-    public var description: String {
-        return "bcrypt error: \(self.reason)"
-    }
-
-    var reason: String {
-        switch self {
-        case .invalidCost:
-            return "Cost should be between 4 and 31"
-        case .invalidSalt:
-            return "Provided salt has the incorrect format"
-        case .hashFailure:
-            return "Unable to compute hash"
-        case .invalidHash:
-            return "Invalid hash formatting"
-        case .internalError:
-            return "Internal bcrypt error"
-        }
-    }
-}
-
 extension Array where Element == UInt8 {
     static func random(count: Int) -> [Element] {
         var array: [Element] = .init(repeating: 0, count: count)
